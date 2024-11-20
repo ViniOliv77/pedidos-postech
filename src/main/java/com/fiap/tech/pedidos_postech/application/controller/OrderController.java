7b75803823e615279125b16d80d5faebd88ef2b9
ViniOliv77 <rm353914@fiap.com.br>
2024-11-20 02:40:32 -0300

    ID#2 Commit

--- src/main/java/com/fiap/tech/pedidos_postech/application/controller/OrderController.java
+++ src/main/java/com/fiap/tech/pedidos_postech/application/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.fiap.tech.pedidos_postech.application.controller;
 
+import com.fiap.tech.pedidos_postech.application.Service.OrderService;
 import com.fiap.tech.pedidos_postech.application.dto.OrderDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/orders")
